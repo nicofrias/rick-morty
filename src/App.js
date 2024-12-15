35a27c9c81e24744165a97d2588ffc7be660aac5
Nico Frias <pnicolasfrias@gmail.com>
2024-12-15 15:55:29 -0300

    subiendo archivos

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import './App.css';
+import imageRickMorty from './img/rick-morty.png';
+import Characters from './components/Characters';
+
+function App() {
+
+  const [characters, setCharacters] = useState(null);
+  
+  const restapi = async () => {
+    const api = await fetch("https://rickandmortyapi.com/api/character");
+    const characterApi = await api.json();
+    setCharacters(characterApi.results);
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className='title'>Rick & Morty</h1>
+        {characters ? (
+          <Characters characters={characters} setCharacters={setCharacters}/>
+          ) : (
+            <>
+              <img src={imageRickMorty} alt='rick and morty' className='img-home'></img>
+              <button onClick={restapi} className='btn-search'>Buscar Personajes</button>
+            </>
+          )
+        }
+        
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
